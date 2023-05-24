552c70df7cf10e76812f5f8def3e2074d8a03c99
Tuguldur666 <>
2023-05-24 22:06:14 +0800

    apox
    done

---
+++ app.js
@@ -0,0 +1,39 @@
+
+function changeImage() {
+    let img = document.getElementById("myImage");
+    let currentImage = img.getAttribute("src");
+    let imageList = [
+      "img/image1.jpg",
+      "img/image2.jpg",
+      "img/image3.jpg"
+    ];
+  
+    let currentIndex = imageList.indexOf(currentImage);
+    let nextIndex = (currentIndex + 1) % imageList.length;
+    let nextImage = imageList[nextIndex];
+  
+    img.src = nextImage;
+  }
+  setInterval(changeImage, 3000);
+  function showImage() {
+    let overlay = document.getElementById("overlay");
+    let overlayImage = document.getElementById("overlayImage");
+    let thumbnailImage = document.getElementById("myImage");
+    let flag = document.querySelectorAll(".flag-icon");
+    overlay.style.display = "flex";
+    flag.forEach(function(icon) {
+        icon.classList.add("fade");
+      });
+    overlayImage.src = thumbnailImage.src;
+  }
+  
+  function hideImage() {
+    let overlay = document.getElementById("overlay");
+    let flag = document.querySelectorAll(".flag-icon");
+    overlay.style.display = "none";
+    flag.forEach(function(icon) {
+        icon.classList.remove("fade");
+      });
+  }
+  
+    
